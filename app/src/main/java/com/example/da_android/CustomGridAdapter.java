40c43a8a8405f9a8b547d6225b51344ea772403c
Thien Phu <phu12062001@gmail.com>
2023-04-24 09:39:34 +0700

    phu test

---
+++ app/src/main/java/com/example/da_android/CustomGridAdapter.java
@@ -0,0 +1,80 @@
+package com.example.da_android;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+
+public class CustomGridAdapter implements ListAdapter {
+    private Context context;
+    private String[] items;
+    LayoutInflater inflater;
+
+    public CustomGridAdapter(Context context, String[] items) {
+        this.context = context;
+        this.items = items;
+        inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int i) {
+        return false;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/da_android/InputTypeChiFragment.java
@@ -0,0 +1,103 @@
+package com.example.da_android;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InputTypeChiFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InputTypeChiFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public InputTypeChiFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment InputTypeChiFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static InputTypeChiFragment newInstance() {
+        InputTypeChiFragment fragment = new InputTypeChiFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    Button btn_lich;
+    GridView gvDanhMuc;
+    Calendar lich = Calendar.getInstance();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_input_type_chi, container, false);
+        btn_lich = view.findViewById(R.id.btn_thoigian);
+        btn_lich.setText(lich.get(Calendar.DAY_OF_MONTH) + "/" +(lich.get(Calendar.MONTH)+1) + "/" +lich.get(Calendar.YEAR));
+        btn_lich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = new DatePickerDialog(view.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                Toast.makeText(getActivity(),day +"___"+month+"___"+year,Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        lich.get(Calendar.YEAR),
+                        lich.get(Calendar.MONTH),
+                        lich.get(Calendar.DAY_OF_MONTH)
+                );
+                dialog.show();;
+            }
+        });
+
+        gvDanhMuc = view.findViewById(R.id.gv_danhmuc);
+
+        String[] item = {"âa","bbb"};
+
+        gvDanhMuc.setAdapter( new CustomGridAdapter(getActivity(), item));
+
+        return view;
+    }
+}
